d4f51ff40bba5b9600c8df8ca561456f08bd0556
Your Name <kanukaharish@gmail.com>
2023-05-24 10:11:17 +0530

    Starting with react app

---
+++ 01.Basics/index.js
@@ -0,0 +1,6 @@
+//Create a element using js and append it to the HTML
+
+// const heading = document.createElement('h2');
+// heading.textContent = 'Hello World from js';
+// heading.className = 'header';
+// document.getElementById('root').append(heading);
